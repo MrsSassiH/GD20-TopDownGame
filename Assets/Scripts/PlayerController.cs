267917d3726b8f581e77bb8b420963a179a17c31
MrsSassiH <70900714+MrsSassiH@users.noreply.github.com>
2021-04-07 13:48:11 +0200

    Adds changes to the player controller script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,16 +2,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    /* 10 * 0.01f = 1/100 -> 10 * (1/100) -> 10/100 -> 0.1
+     * 0.5 -> 1/2
+     * 0.25 -> 1/4
+     * 0.1 -> 1/10
+     * 0.01 -> 1/100
+     */
+
+    public float movementSpeed = 0.1f; 
 
     // Update is called once per frame
     void Update()
     {
-        /*We assignthe Righthand-Side
+       /*We assignthe Righthand-Side
         * - when is the Result of the GetKey-Method
         * - which is of type 'bool'
         * To the Lefthand-Side
@@ -26,11 +29,15 @@ public class PlayerController : MonoBehaviour
             * Transform Moves the Transform in a certain direction
             * Vector3.up is always X: 0, Y: 1, Z: 0
             */
-            transform.Translate(Vector3.up);
-        }
+            transform.Translate(Vector3.up * (movementSpeed * Time.deltaTime));
 
+        }
     }
 
-
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
 }
